2ef85926618391b31dd38fc048971cf874d10785
lucasilmor <lucassilmor0@hotmail.com>
2024-11-02 16:48:04 -0300

    adicionado pasta public

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ connectDB()
 
 app.use(express.json())
 
-
+app.use(express.static('public'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>  console.log(`rodano em http://localhost:${PORT}`))
+
