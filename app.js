46e07698dcf7105a16917a8a40ee3492b5b0fe37
lucasilmor <lucassilmor0@hotmail.com>
2024-11-02 16:27:17 -0300

    conexao com o banco mongo e core do app

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const app = express()
+
+require('dotenv').config();
+
+const connectDB = require('./config/db')
+connectDB()
+
+app.use(express.json())
+
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () =>  console.log(`rodano em http://localhost:${PORT}`))
